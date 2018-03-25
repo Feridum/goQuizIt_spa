d0938dec6803c33c09263f5090541fa8574d50d2
feridum <aleksander.patschek@gmail.com>
2018-03-25 20:50:16 +0200

    Created project, added basic routes and pages

---
+++ goQuizIt/src/app/admin/active-quiz-list/active-quiz-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-active-quiz-list',
+  templateUrl: './active-quiz-list.component.html',
+  styleUrls: ['./active-quiz-list.component.css']
+})
+export class ActiveQuizListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ goQuizIt/src/app/admin/finished-quiz-list/finished-quiz-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-finished-quiz-list',
+  templateUrl: './finished-quiz-list.component.html',
+  styleUrls: ['./finished-quiz-list.component.css']
+})
+export class FinishedQuizListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
