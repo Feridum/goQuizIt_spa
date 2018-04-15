422c4cf376bd42b6dd0c292484df7ee87b50096e
feridum <aleksander.patschek@gmail.com>
2018-04-15 19:15:36 +0200

    added basic styles

--- src/app/admin/quiz-list/quiz-list.component.ts
+++ src/app/admin/quiz-list/quiz-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {logout} from '../../redux/auth/auth.actions';
 import {NgRedux} from '@angular-redux/store';
 import {IAppState} from '../../redux/state.interface';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-quiz-list',
@@ -10,9 +11,10 @@ import {IAppState} from '../../redux/state.interface';
 })
 export class QuizListComponent implements OnInit {
 
-  constructor( private ngRedux: NgRedux<IAppState>) { }
+  constructor( private ngRedux: NgRedux<IAppState>, private router: ActivatedRoute) { }
 
   ngOnInit() {
+    console.log(this.router.snapshot.url);
   }
 
   logout() {
--- src/app/admin/single-quiz-list-element/single-quiz-list-element.component.ts
+++ src/app/admin/single-quiz-list-element/single-quiz-list-element.component.ts
@@ -1,7 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-single-quiz-list-element',
+  selector: '[app-single-quiz-list-element]',
   templateUrl: './single-quiz-list-element.component.html',
   styleUrls: ['./single-quiz-list-element.component.scss']
 })
