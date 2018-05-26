d2977d3e8046009d238bb34c4e8ec8143961365f
Jarosław Bartoszuk <Jaroslaw.Bartoszuk@itSilesia.com>
2018-05-26 23:45:06 +0200

    Welcom page based on updated bootstrap grid.

--- src/app/public/welcome/welcome.component.ts
+++ src/app/public/welcome/welcome.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, AfterContentInit} from '@angular/core';
+import * as Parallax from 'parallax-js';
 
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.scss']
 })
-export class WelcomeComponent implements OnInit {
+export class WelcomeComponent implements OnInit, AfterContentInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
+  ngAfterContentInit() {
+    const logotype = document.getElementsByClassName('logotype-col').item(0);
+    const parallaxLogotypeInstance = new Parallax(logotype);
+    const tokenLogin = document.getElementsByClassName('token-row').item(0);
+    const parallaxTokenLoginInstance = new Parallax(tokenLogin);
+    const tokenQuiz = document.getElementsByClassName('token-row').item(1);
+    const parallaxTokenQuizInstance = new Parallax(tokenQuiz);
+  }
+
 }
+
+
