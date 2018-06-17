a434aaed89c9d99898fbabb820916c97c5508829
feridum <aleksander.patschek@gmail.com>
2018-06-17 12:37:54 +0200

    fix2

--- server.js
+++ server.js
@@ -5,7 +5,10 @@ const app = express();
 const  path = require("path");
 // Run the app by serving the static files
 // in the dist directory
-
+app.use(function(req, res, next) {
+  res.setHeader("Content-Security-Policy", "img-src 'self' data:; default-src 'self' data: *");
+  return next();
+});
 app.use(express.static(__dirname + '/dist'));
 
 // Start the app by listening on the default
