234d9717ef69092a42565585d68b4bc773a064a1
Jarosław Bartoszuk <jarobar435@student.polsl.pl>
2018-06-18 07:47:45 +0200

    Changed player-list into table.

--- src/app/admin/active-quiz-list/active-quiz-list.component.ts
+++ src/app/admin/active-quiz-list/active-quiz-list.component.ts
@@ -1,26 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Observable} from 'rxjs/Observable';
 import {mapToArray} from '../../redux/quiz/quiz.helpers';
 import {NgRedux, select$} from '@angular-redux/store';
-import {IActiveQuiz, IInactiveQuiz} from '../../redux/quiz/quiz.interface';
-import {getActiveQuizList, getInactiveQuizList} from '../../redux/quiz/quiz.actions';
+import {IActiveQuiz} from '../../redux/quiz/quiz.interface';
+import {getActiveQuizList} from '../../redux/quiz/quiz.actions';
 import {IAppState} from '../../redux/state.interface';
-
-const mockData = [
-  {
-    id: '1',
-    name: 'foo1',
-    status: 'active',
-    token: 'token1'
-  },
-  {
-    id: '2',
-    name: 'foo2',
-    status: 'active',
-    token: 'token2'
-  }
-]
-
 
 @Component({
   selector: 'app-active-quiz-list',
@@ -31,7 +15,8 @@ export class ActiveQuizListComponent implements OnInit {
 
   @select$(['quiz', 'activeQuizList'], mapToArray) activeQuizList: Observable<IActiveQuiz[]>;
 
-  constructor(private ngRedux: NgRedux<IAppState>) { }
+  constructor(private ngRedux: NgRedux<IAppState>) {
+  }
 
   ngOnInit() {
     this.activeQuizList.subscribe(quizList => {
